c768cc1c8de7c9b7a00f62750e728f3ab2a5d865
marihachi <marihachi0620@gmail.com>
2018-09-24 16:47:08 +0900

    update

---
+++ src/client/components/app.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<p>minazuki admin page</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		},
+		methods: {
+
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/client/entry.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import app from './components/app';
+
+new Vue({
+	el: '#app',
+	components: { app },
+	template: '<app />'
+});
